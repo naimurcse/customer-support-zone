91cba81f282159c9e859d0fa27357b248579392e
Naimur Rahaman <2015@gmail.com>
2026-03-04 08:45:58 +0600

    Navber Component is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navber from "./Components/Navber/Navber";
 
 function App() {
   return (
     <>
       <div>
+        <Navber></Navber>
         <h1>Vite + React</h1>
       </div>
     </>
