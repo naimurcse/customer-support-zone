20fc5ac33c12aae6a98e82c62125186008571d5d
Naimur Rahaman <2015@gmail.com>
2026-03-04 12:35:10 +0600

    Footer sention is done for larger screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
 import Banner from "./Components/Banner/Banner";
+import Footer from "./Components/Footer/Footer";
 import Navber from "./Components/Navber/Navber";
 
 function App() {
   return (
     <>
-      <div>
-        <Navber></Navber>
-        <Banner></Banner>
-        <h1>Vite + React</h1>
+      <Navber></Navber>
+      <Banner></Banner>
+      <div className="gird gird">
+        <div>Customer Tickets</div>
+        <div> Task Status/Resolved Task</div>
       </div>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,104 @@
+import emailLogo from "../../assets/email-logo.png";
+import fbLogo from "../../assets/fb-logo.png";
+import inLogo from "../../assets/in-logo.png";
+import xLogo from "../../assets/x-logo.png";
+import "./Footer.css";
+function Footer() {
+  return (
+    <footer className="footer-section bg-black text-white mt-15 pt-16 pb-8">
+      {/* Footer Upper Part */}
+      <div className="footer-upper max-w-10/12 mx-auto border-b-1 border-gray-500 pb-8 mb-8">
+        <div className="grid grid-cols-5 md:grid-cols-6 gap-12">
+          <div className="md:col-span-2 md:pr-20">
+            <h3 className="font-bold text-2xl mb-4">CS — Ticket System</h3>
+            <p className="footer-p-text">
+              Our Customer Support Ticket System is designed to simplify and
+              streamline the way your eCommerce business handles customer
+              issues. We provide a structured platform where customers can
+              submit their problems, inquiries, or complaints quickly and
+              efficiently.
+            </p>
+          </div>
+
+          {/* Company */}
+          <div>
+            <h4 className="text-xl font-semibold mb-5">Company</h4>
+            <ul className="not-first:">
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Our Mission</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Services */}
+          <div>
+            <h4 className="text-xl font-semibold mb-5">Services</h4>
+            <ul className="not-first:">
+              <li>
+                <a href="#">Products & Services</a>
+              </li>
+              <li>
+                <a href="#">Customer Stories</a>
+              </li>
+              <li>
+                <a href="#">Download Apps</a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Information */}
+          <div>
+            <h4 className="text-xl font-semibold mb-5">Information</h4>
+            <ul className="not-first:">
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="#">Terms & Conditions</a>
+              </li>
+              <li>
+                <a href="#">Join Us</a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Social Links */}
+          <div>
+            <h4 className="text-xl font-semibold mb-5">Social Links</h4>
+            <ul className="not-first:">
+              <li className="flex gap-2">
+                <img className="w-6" src={xLogo} alt="" />
+                <a href="#">@CS-Ticket System</a>
+              </li>
+              <li className="flex gap-2">
+                <img className="w-6" src={inLogo} alt="" />
+                <a href="#">@CS-Ticket System</a>
+              </li>
+              <li className="flex gap-2">
+                <img className="w-6" src={fbLogo} alt="" />
+                <a href="#">@CS-Ticket System</a>
+              </li>
+              <li className="flex gap-2">
+                <img className="w-6" src={emailLogo} alt="" />
+                <a href="#">support@cst.com</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      {/* Footer Lower Part */}
+      <p className="text-center footer-p-text">
+        © 2026 CS — Ticket System. All rights reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
